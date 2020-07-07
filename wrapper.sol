a78fc2ddab7c3e8373df6936bba54554672ebe4b
Patrick Dehkordi <patrick.dehkordi@gmail.com>
2020-07-07 12:34:39 -0700

    Update wrapper.sol

--- wrapper.sol
+++ wrapper.sol
@@ -1,6 +1,6 @@
-pragma solidity ^0.5.8;
-// IFT
-interface IERC20 { 
+pragma solidity ^0.6.0;
+
+interface iERC20 { 
     function transfer(address to, uint256 value) external returns (bool);
     function approve(address spender, uint256 value) external returns (bool);
     function transferFrom(address from, address to, uint256 value) external returns (bool);
@@ -11,6 +11,7 @@ interface IERC20 {
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
+/***=============== math ============================================================***/
 library SafeMath {
 // Mutiplication
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -20,38 +21,34 @@ library SafeMath {
         uint256 c = a * b;
         require(c / a == b);
         return c;
-    }
-// Division  
+    }  
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b > 0);
         uint256 c = a / b;
         assert(a == b * c + a % b); // Tautalogy 
         return c;
     }
-// Subtract
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b <= a);
         uint256 c = a - b;
         return c;
     }
-// Addition
     function add(uint256 a, uint256 b) internal pure returns (uint256) {
         uint256 c = a + b;
         require(c >= a);
         return c;
     }
-// Modulo
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
         require(b != 0);
         return a % b;
     }
 }
-// ERC20 from FirstBlood implementation 
-contract ERC20 is IERC20 {
+
+/***====================== ERC20 implementation =============================================================***/ 
+contract ERC20 is iERC20 {
     using SafeMath for uint256;
     mapping (address => uint256) private _balances;
     mapping (address => mapping (address => uint256)) private _allowed;
-    
     uint256 private _totalSupply;
     function totalSupply() public view returns (uint256) {
         return _totalSupply;
@@ -59,32 +56,26 @@ contract ERC20 is IERC20 {
     function balanceOf(address owner) public view returns (uint256) {
         return _balances[owner];
     }
-
     function allowance(address owner, address spender) public view returns (uint256) {
         return _allowed[owner][spender];
-    }
-    
+    }    
     function transfer(address to, uint256 value) public returns (bool) {
         _transfer(msg.sender, to, value);
         return true;
-    }
-    
-/*** NEEDS FIX per https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/edit ***/ 
+    }    
+// ===========>>> https://docs.google.com/document/d/1YLPtQxZu1UAvO9cZ1O2RPXBbT0mooh4DYKjA_jp-RLM/edit  
     function approve(address spender, uint256 value) public returns (bool) {
         require(spender != address(0));
         _allowed[msg.sender][spender] = value;
         emit Approval(msg.sender, spender, value);
         return true;
     }
-    
     function transferFrom(address from, address to, uint256 value) public returns (bool) {
         _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
         _transfer(from, to, value);
         emit Approval(from, msg.sender, _allowed[from][msg.sender]);
         return true;
     }
-
-/*** From MonolithDAO Token.sol ***/
     function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
         require(spender != address(0));
         _allowed[msg.sender][spender] = _allowed[msg.sender][spender].add(addedValue);
@@ -98,21 +89,18 @@ contract ERC20 is IERC20 {
         emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
         return true;
     }
-
     function _transfer(address from, address to, uint256 value) internal {
         require(to != address(0));
         _balances[from] = _balances[from].sub(value);
         _balances[to] = _balances[to].add(value);
         emit Transfer(from, to, value);
     }
-
     function _mint(address account, uint256 value) internal {
         require(account != address(0));
         _totalSupply = _totalSupply.add(value);
         _balances[account] = _balances[account].add(value);
         emit Transfer(address(0), account, value);
     }
-
     function _burn(address account, uint256 value) internal {
         require(account != address(0));
         _totalSupply = _totalSupply.sub(value);
@@ -127,7 +115,7 @@ contract ERC20 is IERC20 {
     }
 }
 
-/*** reentrancy attacks guard. function `nonReentrant` is external ***/
+/*** reentrancy =====================================================================***/
 contract ReentrancyGuard {
     uint256 private _guardCounter;
     constructor() public {
